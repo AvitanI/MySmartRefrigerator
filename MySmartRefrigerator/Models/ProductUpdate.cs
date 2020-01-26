0437a127c0444947387d8f86baa0fd87c941011e
AvitanI <Avitanidan@gmail.com>
2020-01-26 18:38:01 +0200

    Add services as DI.

--- MySmartRefrigerator/Models/ProductUpdate.cs
+++ MySmartRefrigerator/Models/ProductUpdate.cs
@@ -2,6 +2,25 @@
 
 namespace MySmartRefrigerator.Models
 {
+    /*
+     * <PriceUpdateDate>2017-02-07 08:54</PriceUpdateDate>
+      <ItemCode>11210000094</ItemCode>
+      <ItemType>1</ItemType>
+      <ItemName>רוטב טבסקו 60 מ"ל</ItemName>
+      <ManufacturerName>ניצן</ManufacturerName>
+      <ManufactureCountry>US</ManufactureCountry>
+      <ManufacturerItemDescription>רוטב טבסקו</ManufacturerItemDescription>
+      <UnitQty>מיליליטרים</UnitQty>
+      <Quantity>60.00</Quantity>
+      <bIsWeighted>0</bIsWeighted>
+      <UnitOfMeasure>100 מ"ל</UnitOfMeasure>
+      <QtyInPackage>0</QtyInPackage>
+      <ItemPrice>12.80</ItemPrice>
+      <UnitOfMeasurePrice>21.33</UnitOfMeasurePrice>
+      <AllowDiscount>1</AllowDiscount>
+      <ItemStatus>1</ItemStatus>
+     */
+
     public class ProductUpdate
     {
         public EChain ChainID { get; set; }
@@ -13,5 +32,15 @@ namespace MySmartRefrigerator.Models
         public string ItemName { get; set; }
 
         public string ItemPrice { get; set; }
+
+        //public string ManufacturerName { get; set; }
+
+        //public string ManufactureCountry { get; set; }
+
+        //public string ManufacturerItemDescription { get; set; }
+
+        //public string UnitQty { get; set; }
+
+        //public decimal Quantity { get; set; }
     }
 }
